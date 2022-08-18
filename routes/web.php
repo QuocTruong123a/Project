3dd0ac1b46c6447b32f6ab955d8d6d52739f2933
QuocTruong123a <102053117+QuocTruong123a@users.noreply.github.com>
2022-08-18 15:58:40 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Login/Login_in');
+});
+Route::prefix('Admin')->group(function(){
+    Route::get('/login',[LoginController::class,'index']);
 });
